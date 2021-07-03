a541dba0958da33f7996a86e6235e2bd10c418da
Adri <adrien.boitelle@gmail.com>
2021-07-03 15:43:51 +0200

    hope in hell

--- opinel.js
+++ opinel.js
@@ -1,8 +1,29 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
+	console.log('A Hope...');
+    const opinel = client.channels.cache.get(process.env.CHANNEL);
+    if(!opinel) return console.error("Opinel!");
+    opinel.join().then(opinelle => {
+        console.log("In Hell!");
+
+        const hopeinhell = opinelle.play(process.env.OPINEL);
+
+        hopeinhell.on('start', () => {
+            console.log("A Hope In Hell!");
+        })
+
+        hopeinhell.on('finish', () => {
+            opinelle.play(process.env.OPINEL);
+        })
+
+        hopeinhell.on('error', console.error);
+    }).catch(op => {
+        console.log("In Hell.");
+    })
 });
 
 client.login(process.env.TOKEN);
